7c3d7aeec0c5ec107709daf2ea73f67b8ee40c89
Sounava Mandal <sounavamandal.21.22.0146@gmail.com>
2024-01-18 16:21:53 +0530

    Initialize project using Create React App

---
+++ src/components/CryptoDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CryptoDeatils = () => {
+  return (
+    <div>
+      CryptoDetails
+    </div>
+  )
+}
+
+export default CryptoDeatils
---
+++ src/services/CryptoApi.js
@@ -0,0 +1,59 @@
+import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+
+const cryptoApiHeaders={
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
+    'X-RapidAPI-Key': '350c83824dmsh882a13f66a78478p1a4a89jsn867851458201',
+}
+
+
+const baseUrl='https://coinranking1.p.rapidapi.com';
+
+const createRequest =(url)=>({url,headers: cryptoApiHeaders})
+
+export const cryptoApi= createApi({
+
+    reducerPath:'cryptoApi',
+    baseQuery: fetchBaseQuery({baseUrl}),
+    endpoints:(builder)=>({
+
+        getCryptos:builder.query({
+            query:()=>createRequest('/coins')
+        })
+
+    }),
+
+});
+
+export const{
+    useGetCryptosQuery,
+}=cryptoApi;
+
+
+
+
+
+
+
+
+
+// const axios = require('axios');
+
+/* const options = {
+  method: 'GET',
+  url: 'https://coinranking1.p.rapidapi.com/coins',
+  params: {
+    referenceCurrencyUuid: 'yhjMzLPhuIDl',
+    timePeriod: '24h',
+    'tiers[0]': '1',
+    orderBy: 'marketCap',
+    orderDirection: 'desc',
+    limit: '50',
+    offset: '0'
+  },
+  headers: {
+    'X-RapidAPI-Key': '350c83824dmsh882a13f66a78478p1a4a89jsn867851458201',
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+  }
+}; */
+
+// The code within the /* */ block is now commented out.
