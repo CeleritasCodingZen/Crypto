93a62ce011b11b85f8827fc666510a8d2139d6e1
Sounava Mandal <sounavamandal.21.22.0146@gmail.com>
2024-01-19 15:23:31 +0530

    Added News

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,15 +1,64 @@
-import React from 'react'
-import{Select,Typography,Row,Col,Avatar,Card} from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Row, Col, Card, Typography, Input } from 'antd';
 import moment from 'moment';
+import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
 
+const { Text, Title } = Typography;
 
+const News = ({ simplified }) => {
+  const { data, isFetching } = useGetCryptoNewsQuery();
+  const [cryptoNews, setCryptoNews] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
-const News = () => {
+  useEffect(() => {
+    if (data && data.data) {
+      setCryptoNews(data.data.slice(0, simplified ? 12 : 27));
+
+      const filteredData = data.data.filter((item) =>
+        item.title.toLowerCase().includes(searchTerm)
+      );
+
+      setCryptoNews(filteredData.slice(0, simplified ? 12 : 27));
+    }
+  }, [data, searchTerm, simplified]);
+
+  if (isFetching) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div>
-      News
+    <div style={{ textAlign: 'center', marginTop: '20px' }}>
+      {!simplified && (
+        <div className="select-news">
+          <Input
+            placeholder="Search News"
+            onChange={(e) => setSearchTerm(e.target.value.toLowerCase())}
+          />
+        </div>
+      )}
+      <Row gutter={[24, 24]}>
+        {cryptoNews.map((news, index) => (
+          <Col xs={24} sm={12} lg={8} key={index}>
+            <Card
+              hoverable
+              className="news-card"
+              cover={<img alt="news" src={news.thumbnail || ''} />}
+            >
+              <a href={news.url} target="_blank" rel="noopener noreferrer">
+                <div className="news-content">
+                  <Title level={4}>{news.title}</Title>
+                  <Text>{news.description}</Text>
+                  <div className="provider-container">
+                    <Text>{moment(news.createdAt).startOf('ss').fromNow()}</Text>
+                  </div>
+                </div>
+              </a>
+            </Card>
+          </Col>
+        ))}
+      </Row>
     </div>
-  )
-}
+  );
+};
 
-export default News
+export default News;
